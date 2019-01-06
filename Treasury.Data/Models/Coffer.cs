cfb292d46d86dc4762917832cfcd8ab044659638
Michael <michaellreagan@gmail.com>
2019-01-05 22:57:21 -0500

    Switch up budget and coffer

--- Treasury.Data/Models/Coffer.cs
+++ Treasury.Data/Models/Coffer.cs
@@ -16,9 +16,23 @@ namespace Treasury.Data.Models
         [Column("NAME")]
         public string Name { get; set; }
 
-
         [MaxLength(1000)]
         [Column("DESCRIPTION")]
         public string Description { get; set; }
+
+        [Column("MONTH")]
+        public Month Month { get; set; }
+
+        [Column("AMOUNT")]
+        public double Amount { get; set; }
+
+        [Column("AMOUNTFUNDED")]
+        public double AmountFunded { get; set; }
+
+        [Column("AMOUNTSPENT")]
+        public double AmountSpent { get; set; }
+
+        [Column("CLOSED", TypeName = "bit")]
+        public Boolean Closed { get; set; }
     }
 }
